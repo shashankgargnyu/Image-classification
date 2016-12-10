f1de46eaa869c94790e70d477699fc1d87ffaa72
shashankgargnyu <sg4437@nyu.edu>
2016-12-10 15:39:19 -0500

    updated mega_histogram.m

--- src/mega_histogram.m
+++ src/mega_histogram.m
@@ -8,15 +8,18 @@ load('image_count.mat')
 
 %%
 k=100;          % Change K=number of clusters
-mega_hist=[];
+mega_hist_new=[];
+first_index=1;
+l_tot=0;
 for i=1:102
-    category_name=names{i};
-    first_index=1;
+    category_name=names{i}
     for j=1:images_count(i)
-        last_index=length(feature_vectors([category_name num2str(j)]));
+        l=size(feature_vectors([category_name num2str(j)]))
+        l_tot=l_tot+l(2);
+        last_index=first_index + l(2) - 1;
         features_im=A(first_index:last_index);
         hist_counts=histcounts(features_im,k);
-        mega_hist=[mega_hist;hist_counts];
+        mega_hist_new=[mega_hist_new;hist_counts];
         first_index=last_index+1;
     end
 end
